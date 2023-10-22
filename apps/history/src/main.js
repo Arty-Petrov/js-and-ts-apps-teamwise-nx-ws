6a1911e9001bffab84538e63799dec0df4ab6dc8
Artyom Petrov <artyom.petrov@gmail.com>
2023-10-22 12:20:07 +0300

    feat(validation) Set main.js/ts globalPipes with ZodValidationPipe

--- apps/history/src/main.js
+++ apps/history/src/main.js
@@ -1,17 +1,20 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app/app.module';
+import { ZodValidationPipe } from 'nestjs-zod';
+import { HistoryModule } from './app/history.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableShutdownHooks();
-  await app.listen(3100);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.HISTORY_APP_PORT || 3100;
-
-  Logger.log(
-    `🚀 Application History is running on: http://localhost:${port}/${globalPrefix}`
-  );
+    const app = await NestFactory.create(HistoryModule);
+    const globalPrefix = 'api';
+    app
+        .setGlobalPrefix(globalPrefix)
+        .enableShutdownHooks()
+        .useGlobalPipes(new ZodValidationPipe())
+    const port = process.env.HISTORY_APP_PORT || 3100;
+    await app.listen(port);
+    Logger.log(
+        `🚀 Application History is running on: http://localhost:${port}/${globalPrefix}`,
+    );
 }
+
 bootstrap();
--- apps/users/src/main.ts
+++ apps/users/src/main.ts
@@ -1,17 +1,20 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { ZodValidationPipe } from 'nestjs-zod';
 
-import { AppModule } from './app/app.module';
+import { UsersModule } from './app/users.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableShutdownHooks();
+  const app = await NestFactory.create(UsersModule);
   const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
+  app
+    .setGlobalPrefix(globalPrefix)
+    .enableShutdownHooks()
+    .useGlobalPipes(new ZodValidationPipe())
   const port = process.env.USERS_APP_PORT || 3000;
   await app.listen(port);
   Logger.log(
-    `🚀 Application Users is running on: http://localhost:${port}/${globalPrefix}`
+    `🚀 Application Users is running on: http://localhost:${port}/${globalPrefix}`,
   );
 }
 
