85dc640bbc45c37f26e619dab2a9ccab4f49941a
Artyom Petrov <artyom.petrov@gmail.com>
2023-10-22 14:55:10 +0300

    feat(specification) Install nestjs/swagger and configure Swagger module in main.js/ts bootstrap

--- apps/history/src/main.js
+++ apps/history/src/main.js
@@ -1,6 +1,7 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { ZodValidationPipe } from 'nestjs-zod';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { patchNestJsSwagger, ZodValidationPipe } from 'nestjs-zod';
 import { HistoryModule } from './app/history.module';
 
 async function bootstrap() {
@@ -9,7 +10,16 @@ async function bootstrap() {
     app
         .setGlobalPrefix(globalPrefix)
         .enableShutdownHooks()
-        .useGlobalPipes(new ZodValidationPipe())
+        .useGlobalPipes(new ZodValidationPipe());
+
+    const config = new DocumentBuilder()
+        .setTitle('History API')
+        .setVersion('1.0')
+        .build();
+    patchNestJsSwagger();
+    const document = SwaggerModule.createDocument(app, config,);
+    SwaggerModule.setup('spec', app, document);
+
     const port = process.env.HISTORY_APP_PORT || 3100;
     await app.listen(port);
     Logger.log(
--- apps/users/src/main.ts
+++ apps/users/src/main.ts
@@ -1,6 +1,7 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { ZodValidationPipe } from 'nestjs-zod';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { patchNestJsSwagger, ZodValidationPipe } from 'nestjs-zod';
 
 import { UsersModule } from './app/users.module';
 
@@ -10,9 +11,19 @@ async function bootstrap() {
   app
     .setGlobalPrefix(globalPrefix)
     .enableShutdownHooks()
-    .useGlobalPipes(new ZodValidationPipe())
+    .useGlobalPipes(new ZodValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('Users API')
+    .setVersion('1.0')
+    .build();
+  patchNestJsSwagger();
+  const document = SwaggerModule.createDocument(app, config,);
+  SwaggerModule.setup('spec', app, document);
+
   const port = process.env.USERS_APP_PORT || 3000;
   await app.listen(port);
+
   Logger.log(
     `🚀 Application Users is running on: http://localhost:${port}/${globalPrefix}`,
   );
